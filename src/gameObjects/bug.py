609af60e52c9c4fa0bf62314191447ce3a975dc6
FatinShadab <macrotech21@gmail.com>
2024-04-10 22:36:11 +0600

    Created classes to represent Game objects

---
+++ src/gameObjects/bug.py
@@ -0,0 +1,9 @@
+from typing import Tuple
+from pygame.sprite import Group
+from . import DropAbleSpriteBaseClass
+
+
+class Bug(DropAbleSpriteBaseClass):
+    def __init__(self, imagesSrc: Tuple[str], groups: Group, windowWH: Tuple[int], scaleable: bool, scaleValue: float, animate: bool, damage : int, randomFactor: Tuple[int] = None) -> None:
+        super().__init__(imagesSrc, groups, windowWH, scaleable, scaleValue, animate, randomFactor)
+        self.damage : int = damage
---
+++ src/gameObjects/fruit.py
@@ -0,0 +1,9 @@
+from typing import Tuple
+from pygame.sprite import Group
+from . import DropAbleSpriteBaseClass
+
+
+class Fruit(DropAbleSpriteBaseClass):
+    def __init__(self, imagesSrc: Tuple[str], groups: Group, windowWH: Tuple[int], scaleable: bool, scaleValue: float, animate: bool, point : int, randomFactor: Tuple[int] = None) -> None:
+        super().__init__(imagesSrc, groups, windowWH, scaleable, scaleValue, animate, randomFactor)
+        self.point : int = point
