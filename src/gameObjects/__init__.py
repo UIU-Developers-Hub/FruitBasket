b6855007ccd1b656ef12d82b48b062a7bbdf3c22
FatinShadab <macrotech21@gmail.com>
2024-04-11 16:34:46 +0600

    Update __init__.py

--- src/gameObjects/__init__.py
+++ src/gameObjects/__init__.py
@@ -1,4 +1,11 @@
+# Importing all objects from the 'abstract' module or file in the current package or directory.
 from .abstract import *
+
+# Importing all objects from the 'bug' module or file in the current package or directory.
 from .bug import *
+
+# Importing all objects from the 'fruit' module or file in the current package or directory.
 from .fruit import *
+
+# Importing all objects from the 'busket' module or file in the current package or directory.
 from .busket import *
