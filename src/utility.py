42fe7af7f0dd949eeaa4cd9bdda7fbb3d3c47bfb
FatinShadab <macrotech21@gmail.com>
2024-04-11 08:42:50 +0600

    Create utility.py
    This file will contain the static data of the project

---
+++ src/utility.py
@@ -0,0 +1,10 @@
+windowConfig = (480, 720)
+fontSize = 50
+folderPaths = {
+    "fruit" : "assets/fruits/",
+    "bug" : "assets/bugs/",
+}
+filePaths = {
+    "font" : "assets/font/Pixeltype.ttf",
+    "bg" : "assets/bg/bg.jpg"
+}
