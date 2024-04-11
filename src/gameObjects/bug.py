98f196eeee8ac496d1d7911b19c9c29b5f39cf62
Fatin Shadab <71595077+FatinShadab@users.noreply.github.com>
2024-04-11 12:12:32 +0600

    Update bug.py

--- src/gameObjects/bug.py
+++ src/gameObjects/bug.py
@@ -4,6 +4,9 @@ from . import DropAbleSpriteBaseClass
 
 
 class Bug(DropAbleSpriteBaseClass):
-    def __init__(self, imagesSrc: Tuple[str], groups: Group, windowWH: Tuple[int], scaleable: bool, scaleValue: float, animate: bool, damage : int, randomFactor: Tuple[int] = None) -> None:
+    def __init__(self, imagesSrc: Tuple[str], groups: Group, windowWH: Tuple[int], scaleable: bool, scaleValue: float, animate: bool, damage : int, gravity : int = None, randomFactor: Tuple[int] = None) -> None:
         super().__init__(imagesSrc, groups, windowWH, scaleable, scaleValue, animate, randomFactor)
         self.damage : int = damage
+        
+        if gravity:
+            self.gravity = gravity
