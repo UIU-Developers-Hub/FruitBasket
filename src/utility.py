ddd2a0f83e39190e093d54b453a3cdc69faede9c
Fatin Shadab <71595077+FatinShadab@users.noreply.github.com>
2024-04-11 12:11:37 +0600

    Update utility.py
    Added variables for level wise parameter control system

--- src/utility.py
+++ src/utility.py
@@ -1,10 +1,52 @@
 windowConfig = (480, 720)
+
 fontSize = 50
+
 folderPaths = {
     "fruit" : "assets/fruits/",
     "bug" : "assets/bugs/",
 }
+
 filePaths = {
     "font" : "assets/font/Pixeltype.ttf",
-    "bg" : "assets/bg/bg.jpg"
+    "bg" : "assets/bg/bg.jpg",
+    "player" : "assets/player/basket.png"
+}
+
+levelWiseParameters = {
+    1 : {
+        "fruitG" : 330,
+        "bugG" : 333,
+        "playerSpd" : 10,
+        "fruitSpwanLimit" : 4,
+        "bugSpwanLimit" : 2
+    },
+    2 : {
+        "fruitG" : 390,
+        "bugG" : 363,
+        "playerSpd" : 13,  
+        "fruitSpwanLimit" : 5,
+        "bugSpwanLimit" : 3
+    },
+    3 : {
+        "fruitG" : 400,
+        "bugG" : 393,
+        "playerSpd" : 16,
+        "fruitSpwanLimit" : 5,
+        "bugSpwanLimit" : 4
+    },
+    4 : {
+        "fruitG" : 420,
+        "bugG" : 403,
+        "playerSpd" : 19,
+        "fruitSpwanLimit" : 5,
+        "bugSpwanLimit" : 4
+    },
+    5 : {
+        "fruitG" : 530,
+        "bugG" : 453,
+        "playerSpd" : 22,
+        "fruitSpwanLimit" : 5,
+        "bugSpwanLimit" : 5  
+    }
 }
