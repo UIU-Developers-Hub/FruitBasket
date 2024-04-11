b60503722548f86fc9015cfa13516c9b28fce532
Fatin Shadab <71595077+FatinShadab@users.noreply.github.com>
2024-04-11 12:12:23 +0600

    Update fruit.py

--- src/gameObjects/fruit.py
+++ src/gameObjects/fruit.py
@@ -4,6 +4,9 @@ from . import DropAbleSpriteBaseClass
 
 
 class Fruit(DropAbleSpriteBaseClass):
-    def __init__(self, imagesSrc: Tuple[str], groups: Group, windowWH: Tuple[int], scaleable: bool, scaleValue: float, animate: bool, point : int, randomFactor: Tuple[int] = None) -> None:
+    def __init__(self, imagesSrc: Tuple[str], groups: Group, windowWH: Tuple[int], scaleable: bool, scaleValue: float, animate: bool, point : int, gravity : int = None, randomFactor: Tuple[int] = None) -> None:
         super().__init__(imagesSrc, groups, windowWH, scaleable, scaleValue, animate, randomFactor)
         self.point : int = point
+        
+        if gravity:
+            self.gravity = gravity
